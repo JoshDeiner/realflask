ac192c3908eada3e942f560bf848a32a13b6a004
Josh Deiner <jepilepsy73@gmail.com>
2019-05-19 10:59:52 -0400

    round two

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+''' render routes'''
 import app
 
-@app.app.route('/')
-@app.app.route('/index')
+@app.app.route("/")
+@app.app.route("/index")
 def index():
-    return "hello world!"
-
+    ''' return html user info '''
+    user = {"username": "miguel"}
+    return (
+        """
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, """
+        + user["username"]
+        + """!</h1>
+        </body>
+    </html>"""
+    )
